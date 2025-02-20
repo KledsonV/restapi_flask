ad0a3aa3841b8b0b0d536ce3426bc276ad7c1d15
Kledson Vinicius <kledsonvinicius0702@gmail.com>
2025-02-20 11:13:03 -0300

    fix: formatting according to flake8

--- app.py
+++ app.py
@@ -4,16 +4,19 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+
 class Users(Resource):
     def get(self):
-        return { "statusCode": 200, "message": "list users" }
-    
+        return {"statusCode": 200, "message": "list users"}
+
+
 class User(Resource):
     def post(self):
-        return { "statusCode": 200, "message": "user[0]" }
-    
+        return {"statusCode": 200, "message": "user[0]"}
+
     def get(self, cpf):
-        return { "statusCode": 200, "message": "CPF: " + cpf }
+        return {"statusCode": 200, "message": "CPF: " + cpf}
+
 
 api.add_resource(Users, "/users")
 api.add_resource(User, "/user", "/user/<string:cpf>")
